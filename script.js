a12aa496afa1b7e5d75d46df3eaf9e800fe8eb53
DiegoArruda <arrudadiego1@gmail.com>
2025-05-08 14:00:15 -0300

    Alteração de lógica do código

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-function buttonClick() {
-	alert("Voce clicou no botao");
-}
+let count = 0;
+const button = document.getElementById("custom-button");
+button.addEventListener("click", function () {
+	count += 1;
+	button.textContent = "Cliques: " + count;
+});
 
 const form = document.getElementById("new-form");
-
 form.addEventListener("submit", function (event) {
 	event.preventDefault();
 	const nome = document.getElementById("name").value;
 	console.log("Nome: " + nome);
+	document.getElementById("name").value = "";
 });
